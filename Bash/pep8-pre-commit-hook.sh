a44d66df3d945f0134a4582381c2a7122d1dd974
mic4ael <crossner90@gmail.com>
2016-07-24 12:39:57 +0200

    Simple pre-commit hook on python files

---
+++ Bash/pep8-pre-commit-hook.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e;
+
+if [[ `which pep8 > /dev/null` -ne 0 ]];
+then
+    echo "PEP8 not installed";
+    echo "Skipping the check";
+    exit 0;
+fi;
+
+for MODIFIED_FILE in `git status --porcelain | cut -d " " -f3`;
+do
+    exec pep8 --max-line-length=120 $MODIFIED_FILE;
+done;
