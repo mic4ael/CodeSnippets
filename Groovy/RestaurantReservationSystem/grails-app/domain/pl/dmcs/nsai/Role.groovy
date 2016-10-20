0bea42196a839d9b364d8f9876c55ec7b2df4e73
mic4ael <crossner90@gmail.com>
2016-10-20 19:23:51 +0200

    Modify User and Role classes to conform to spring security interface

--- Groovy/RestaurantReservationSystem/grails-app/domain/pl/dmcs/nsai/Role.groovy
+++ Groovy/RestaurantReservationSystem/grails-app/domain/pl/dmcs/nsai/Role.groovy
@@ -1,7 +1,18 @@
 package pl.dmcs.nsai
 
+import groovy.transform.ToString
+import groovy.transform.EqualsAndHashCode
+
+@EqualsAndHashCode(includes='authority')
+@ToString(includes='authority', includeNames=true, includePackage=true)
 class Role {
+    String authority
 
     static constraints = {
+        authority(blank: false, unique: true)
+    }
+
+    static mapping = {
+        cache(true)
     }
 }
--- Groovy/RestaurantReservationSystem/grails-app/domain/pl/dmcs/nsai/User.groovy
+++ Groovy/RestaurantReservationSystem/grails-app/domain/pl/dmcs/nsai/User.groovy
@@ -1,7 +1,51 @@
 package pl.dmcs.nsai
 
-class User {
+import groovy.transform.ToString
+import groovy.transform.EqualsAndHashCode
+
+@EqualsAndHashCode(includes='username')
+@ToString(includes='username', includeNames=true, includePackage=true)
+class User implements Serializable {
+    transient springSecurityService
+
+    static transients = ['springSecurityService']
+
+    static mapping = {
+        password(column: '`password`')
+    }
 
     static constraints = {
+        password(blank: false, password: true)
+        username(blank: false, unique: true)
+    }
+
+    String username
+    String password
+
+    boolean enabled
+    boolean accountExpired
+    boolean accountLocked
+    boolean passwordExpired
+
+    Set<Role> getAuthorities() {
+        UserRole.findAllByUser(this)*.role
+    }
+
+    def beforeInsert() {
+        encodePassword()
+    }
+
+    def beforeUpdate() {
+        if (isDirty('password')) {
+            encodePassword()
+        }
+    }
+
+    protected void encodePassword() {
+        if (springSecurityService?.passwordEncoder) {
+            password = springSecurityService.encodePassword(password)
+        } else {
+            password = password
+        }
     }
 }
