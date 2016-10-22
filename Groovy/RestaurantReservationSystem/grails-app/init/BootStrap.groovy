09feaa04234233059c2f9491d10985744311add3
mic4ael <crossner90@gmail.com>
2016-10-22 10:36:55 +0200

    Create user roles and an admin on Application init

---
+++ Groovy/RestaurantReservationSystem/grails-app/init/BootStrap.groovy
@@ -0,0 +1,18 @@
+import pl.dmcs.nsai.Role
+import pl.dmcs.nsai.User
+import pl.dmcs.nsai.UserRole
+
+class BootStrap {
+   def init = {
+      def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+      def userRole = new Role(authority: 'ROLE_USER').save()
+
+      def testUser = new User(username: 'admin', password: 'admin').save()
+
+      UserRole.create(testUser, adminRole)
+      UserRole.withSession {
+         it.flush()
+         it.clear()
+      }
+   }
+}
