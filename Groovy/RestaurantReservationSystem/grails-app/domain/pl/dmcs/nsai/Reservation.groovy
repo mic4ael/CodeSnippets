486e0f5dfec353c4211f0cc9b0bd0ecb2aad7d04
mic4ael <crossner90@gmail.com>
2016-11-04 14:10:31 +0100

    Add column reserved_at

--- Groovy/RestaurantReservationSystem/grails-app/domain/pl/dmcs/nsai/Reservation.groovy
+++ Groovy/RestaurantReservationSystem/grails-app/domain/pl/dmcs/nsai/Reservation.groovy
@@ -3,10 +3,12 @@ package pl.dmcs.nsai
 
 class Reservation {
     Date createdAt
+    Datetime reservedAt
 
     static belongsTo = [createdBy: User, reservedTable: Table]
     static mapping = {
         table('reservations')
         createdAt(column: 'created_at', defaultValue: 'NOW()')
+        reservedAt(column: 'reserved_at')
     }
 }
