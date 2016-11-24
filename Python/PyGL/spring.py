73cfdd24444c1467b2fba9310f8ed8e10b8973e0
mic4ael <crossner90@gmail.com>
2016-11-24 16:21:30 +0100

    Initial file for spring model in OpenGL

---
+++ Python/PyGL/spring.py
@@ -0,0 +1,31 @@
+# 1. Pamiętaj o możliwości zmiany perspektywy
+#     przy użyciu strzałek
+# 2. Nałożenie tekstur
+# 3. Ruch kulki według wzoru z instrukcji
+# 4. Budowanie sprężyny z nachodzących na siebie kulek
+
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+
+def init():
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glClearDepth(1.0)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+
+
+def display():
+    pass
+
+
+if __name__ == '__main__':
+    glutInit()
+    glutInitWindowSize(640, 480)
+    glutCreateWindow('Spring')
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+    glutDisplayFunc(display)
+    glutIdleFunc(display)
+    init()
+    glutMainLoop()
