a2124ea823983a4938f86bb39a48a144003b9341
mic4ael <crossner90@gmail.com>
2016-12-23 19:10:44 +0100

    Increase size of the generated file

--- JAVA/PTOiSH/Tasks/src/main/java/pl/dmcs/ptoish/exercise1/Exercise1b.java
+++ JAVA/PTOiSH/Tasks/src/main/java/pl/dmcs/ptoish/exercise1/Exercise1b.java
@@ -6,7 +6,7 @@ import pl.dmcs.ptoish.benchmarking.BenchmarkRunner;
 public class Exercise1b {
     public static void main(String []args) {
         BenchmarkRunner benchmarkRunner = new BenchmarkRunner("pl.dmcs.ptoish.exercise1");
-        FileGenerator generatedFile = new FileGenerator("random_data.txt", 200);
+        FileGenerator generatedFile = new FileGenerator("random_data.txt", 500);
 
         System.out.println("Generating a file with random data");
         generatedFile.generate();
