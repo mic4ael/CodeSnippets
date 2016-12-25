512fc69c01f11b1505f06e06c5fb7a33f5a7dcf4
mic4ael <crossner90@gmail.com>
2016-12-25 12:06:52 +0100

    Add different types o objects to the cache

--- JAVA/PTOiSH/Tasks/src/main/java/pl/dmcs/ptoish/exercise3/HashMapCacheBenchmark.java
+++ JAVA/PTOiSH/Tasks/src/main/java/pl/dmcs/ptoish/exercise3/HashMapCacheBenchmark.java
@@ -1,5 +1,7 @@
 package pl.dmcs.ptoish.exercise3;
 
+import java.util.LinkedList;
+
 import pl.dmcs.ptoish.benchmarking.BenchmarkClass;
 import pl.dmcs.ptoish.benchmarking.BenchmarkMethod;
 
@@ -11,16 +13,32 @@ public class HashMapCacheBenchmark {
     @BenchmarkMethod(numberOfIterations=1000, logfile="cache_put.txt")
     public static void writingToCacheBenchmar() {
         cache.put("test", 123456);
+        cache.put("test1", "test");
+        cache.put("test2", 2.0f);
+        cache.put("test3", new Object());
+        cache.put("test4", new LinkedList<Integer>());
+        cache.put("test5", new Integer(15));
+        cache.put("test5", new int[15]);
     }
 
     @BenchmarkMethod(numberOfIterations=1000, logfile="cache_read.txt")
     public static void readingFromCacheBenchmark() {
         cache.get("test");
+        cache.get("test1");
+        cache.get("test2");
+        cache.get("test3");
+        cache.get("test4");
+        cache.get("test5");
     }
 
 
     @BenchmarkMethod(numberOfIterations=1000, logfile="cache_delete.txt")
     public static void removingKeyFromCacheBenchmark() {
         cache.clear("test");
+        cache.clear("test1");
+        cache.clear("test2");
+        cache.clear("test3");
+        cache.clear("test4");
+        cache.clear("test5");
     }
 }
